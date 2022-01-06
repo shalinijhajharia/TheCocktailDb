b3860c19638b5690d8fdab58c80ad041faac1826
shalini jhajharia <shalinijhajharia185@gmail.com>
2022-01-06 21:37:21 +0530

    Updates

---
+++ precache-manifest.ee2c1a0374a2de179e1d96bdda7dbfe2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24d10a45cb7ba3053dfbfc99e96d702",
+    "url": "/theCocktaildb/index.html"
+  },
+  {
+    "revision": "3c26789004cc4396dc35",
+    "url": "/theCocktaildb/static/css/main.2eb3e740.chunk.css"
+  },
+  {
+    "revision": "264eeb5c0c3d58f6cc58",
+    "url": "/theCocktaildb/static/js/2.25823bee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/theCocktaildb/static/js/2.25823bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c26789004cc4396dc35",
+    "url": "/theCocktaildb/static/js/main.2bab52c5.chunk.js"
+  },
+  {
+    "revision": "983588daf956251277d3",
+    "url": "/theCocktaildb/static/js/runtime-main.c35b6a71.js"
+  },
+  {
+    "revision": "8060cb7dbdc174835abd2d295d42ca4f",
+    "url": "/theCocktaildb/static/media/finger.8060cb7d.svg"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/theCocktaildb/static/media/logo.f3ad7664.svg"
+  }
+]);
