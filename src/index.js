612207385061111fc2b874f821cb53d1729710cb
shalini jhajharia <shalinijhajharia185@gmail.com>
2022-01-06 21:25:50 +0530

    ProGrad ID 5665

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { AppProvider } from './context'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <AppProvider>
     <App />
     </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
